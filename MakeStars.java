444c50f478b54caf2035c048753794ae778164b4
diana_yang <dyang20@stuy.edu>
2020-12-23 13:58:37 -0500

    changed so that whitespace isn't preserved

--- MakeStars.java
+++ MakeStars.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class MakeStars{
   public static void main(String[] args){
     Scanner n = new Scanner(System.in);
-    while (n.hasNextLine()){
+    /*while (n.hasNextLine()){
       String line = n.nextLine();
       for (int i=0; i<line.length(); i++){
         if(line.charAt(i)== ' '){
@@ -12,6 +12,18 @@ public class MakeStars{
         else{
           System.out.print("*");
         }
+      }
+      System.out.println();
+    }*/ //considers whitespace
+
+    while (n.hasNextLine()){
+      Scanner line = new Scanner(n.nextLine());
+      while(line.hasNext()){
+        String word = line.next();
+        for (int i=0; i<word.length(); i++){
+          System.out.print("*");
+        }
+        System.out.print(" ");
       }
       System.out.println();
     }
